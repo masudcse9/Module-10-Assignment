f48887664247fe89c65c40a960022aa6cc4b3923
masudcse9 github <masudcse9@gmail.com>
2023-08-09 23:54:51 +0600

    first commit

---
+++ src/app/Description/page.js
@@ -0,0 +1,23 @@
+import React from "react";
+
+const page = () => {
+  return (
+    <div>
+      <h1>Description :</h1>
+      <p>
+        package.json : Hoche ar modde somostho package gulo add thakbe. And
+        future a ja ja package install korbo ta akhane pabo.
+      </p>
+      <p>next.config.js : Ae modde amra amader moto kore build hobar somoy file name change korte parbo.
+        aro anek kichu korte parbo.
+      </p>
+      <p>layout.js : Ar ta hoche Global layout ja amra prothita page a pabo.</p>
+      <p>page.js : Ar ta hoche home page ja amra prothita page a pabo.</p>
+      <p>src directory: Ar ta hoche app directory.Jar modde interial application thakbe. Fac icon and global css thakbe.</p>
+      <p>src/app directory: Ar ta hoche app directory.Jar modde interial application thakbe.</p>
+      <p>public directory: Ar ta application ar icon or images thakbe.</p>
+    </div>
+  );
+};
+
+export default page;
